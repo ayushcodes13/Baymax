a9bfa80ffe49b7a380b1735015c68a85fa190b02
Devayush Rout <devayushrout@gmail.com>
2025-07-20 23:51:04 +0530

    added requirements

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "baymax",
+    version = "0.1.0",
+    author = "Devayush Rout",
+    author_email = "devayushrout@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
