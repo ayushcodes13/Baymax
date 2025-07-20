e7422b71c6d59d2ade37498a93a9a516575a33b4
Devayush Rout <devayushrout@gmail.com>
2025-07-20 22:06:51 +0530

    Directory and files created

---
+++ template.sh
@@ -0,0 +1,17 @@
+#Creating Directory
+mkdir -p src
+mkdir -p research
+
+#Creating Files
+torch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch requirements.txt
+touch research/trials.ipynb
+
+
+
+echo "Directory and files created successfully."
